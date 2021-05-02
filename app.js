83564fc581b944cbc5cd848c5eb63e3ac45a401e
MOMOJMOGG <momomacaca@gmail.com>
2021-05-02 23:27:09 +0800

    feat: initialize project

---
+++ app.js
@@ -0,0 +1,29 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// setting static files
+app.use(express.static('public'))
+
+// body parser 
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// routes setting
+app.get('/', (req, res) => {
+  res.send('This is my movie list built with Express')
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
